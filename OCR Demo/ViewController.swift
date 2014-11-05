2e5b4c46ea278ee727a6e9e8b9ad953270e8b9ee
bpolat <bpolat@live.com>
2014-11-05 23:26:22 +0200

    Initial Commit

---
+++ OCR Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OCR Demo
+//
+//  Created by polat on 05/11/14.
+//  Copyright (c) 2014 polat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
